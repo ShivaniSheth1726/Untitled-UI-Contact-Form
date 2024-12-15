b992e726ca451a37b6434197c7fd1cfd3a888f44
Shivani Sheth Mehta <shethshivani1726@gmail.com>
2024-12-15 07:25:42 +0000

    home page basic responsiveness added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import Header from '@/components/Header'
-import Aside from '@/components/Aside'
-import Form from '@/components/Form'
+import React from "react";
+import Header from "@/components/Header";
+import Aside from "@/components/Aside";
+import Form from "@/components/Form";
+
 const Home = () => {
   return (
-    <div>
-        <Header/>
-        <Aside/>
-        <Form/>
-    </div>
-  )
-}
+    <div className="p-4 w-full">
+      <Header />
 
-export default Home
+      <div className="grid grid-cols-8">
+        <div className="bg-green-300 col-span-2 hidden md:block">
+          <Aside />
+        </div>
+        <div className="md:bg-lime-400 md:col-span-6 col-span-full">
+          <Form />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
