36d6f4b3c0a26de0427836a77ecc74b779bdfc65
Shivani Sheth Mehta <shethshivani1726@gmail.com>
2024-12-15 07:06:57 +0000

    structure crerated and alias created for path

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { createRoot} from "react-dom/client";
+import "./index.css";
+import Home from "@/pages/Home";
+
 const rootEl =document.querySelector("#root");
 const root = createRoot(rootEl);
 root.render(
     <React.StrictMode>
-        <h1>Hello</h1>
+       <Home/>
     </React.StrictMode>
 );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '@/components/Header'
+import Intro from '@/components/Intro'
+import Aside from '@/components/Aside'
+import Form from '@/components/Form'
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
